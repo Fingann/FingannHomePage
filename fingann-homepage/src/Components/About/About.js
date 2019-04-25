cfff7e15758b0743a843728717c080916ba45aab
Fingann <Fingann92@gmail.com>
2019-04-25 08:45:52 +0200

    Fixed centering and overflow

--- fingann-homepage/src/Components/About/About.js
+++ fingann-homepage/src/Components/About/About.js
@@ -5,13 +5,16 @@ import ProfilePicture from '../ProfilePicture/ProfilePicture';
 class About extends Component {
     constructor(props){
         super(props);
+        
         this.state = ({DisplayTitle: "About"});
     }
     render() {
       return (
             <div className="about">
-              <ProfilePicture className="profile"/>
-              <div className="info"><p>Test</p></div>
+              {/* <ProfilePicture className="profile"/> */}
+              <div className="firstColumn"><p>Test</p></div>
+              <div className="secoundColumn"><p>Test</p></div>
+
             </div>
       );
     }
